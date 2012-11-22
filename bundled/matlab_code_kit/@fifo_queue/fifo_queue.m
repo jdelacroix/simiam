715b29d2d39a852598eb1a6a35bfc4c38fa4c2e9
Jean-Pierre de la Croix <ajpl.delacroix@gmail.com>
2012-11-22 12:19:43 -0500

    Added parts of the matlab_code_kit to the bundled software.

---
+++ bundled/matlab_code_kit/@fifo_queue/fifo_queue.m
@@ -0,0 +1,28 @@
+classdef fifo_queue < dl_list
+
+% Copyright (C) 2012 Jean-Pierre de la Croix
+% see the LICENSE file included with this software
+ 
+    properties
+        
+    end
+    
+    methods
+        function obj = fifo_queue()
+            obj = obj@dl_list();
+        end
+        
+        function enqueue(obj, key)
+            obj.insert_key(key, 1);
+        end
+        
+        function key = dequeue(obj)
+            key = obj.remove_key(obj.size_);
+        end
+        
+        function bool = empty(obj)
+            bool = (obj.size_ == 0);
+        end
+    end
+    
+end
---
+++ bundled/matlab_code_kit/README.md
@@ -0,0 +1,13 @@
+matlab_code_kit
+===============
+
+A kit of useful MATLAB code, such as doubly-linked lists, FIFO queues, random rapidly-exploring trees, and quad trees.
+
+Contents:
+---------
+
++ Doubly-linked list (@dl_list_*)
++ First In, First Out queue (@fifo_queue)
++ Quad Tree (@quad_tree_*)
++ Left Child, Right Sibling tree (@lcrs_tree_*)
++ Rapidly-exploring dense tree (@r_dense_tree)
