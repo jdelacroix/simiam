436c9484cc9be455c087cc2b21be733be3976846
Jean-Pierre de la Croix <ajpl.delacroix@gmail.com>
2015-07-26 08:57:58 -0700

    Modified UI to leverage GUI Layout Toolbox and HG2.

--- +simiam/+ui/AppWindow.m
+++ +simiam/+ui/AppWindow.m
@@ -8,6 +8,10 @@ classdef AppWindow < handle
         layout_
         view_
         
+        status_ui_
+        map_ui_
+        controls_ui_
+        
         ui_colors_
         ui_size_
         ui_buttons_
@@ -109,16 +113,11 @@ classdef AppWindow < handle
             set(obj.parent_, 'Renderer', 'zBuffer');
 
             % Create user interface (ui) layout
-            obj.layout_ = GridLayout(obj.parent_, 'RowHeight', {24,'*','*',36}, ....
-                                                  'ColWidth', {48, 48, 48, '*' ,64, 96, 64,'*', 48 , 48, 48}, ...,
-                                                  'LMargin', 10, 'RMargin', 6, ...
-                                                  'Gap', 5, ...
-                                                  'CellColor', obj.ui_colors_.gray);
-
-            set(obj.layout_.Container, 'BackgroundColor', obj.ui_colors_.gray);
-            MergeCells(obj.layout_, [2 3], [1 11]);            
-            Update(obj.layout_);
-            
+            obj.layout_ = uix.VBox('Parent', obj.parent_, 'BackgroundColor', obj.ui_colors_.gray);
+            obj.status_ui_ = uix.HBox('Parent', obj.layout_, 'BackgroundColor', [0 0 0]);
+            obj.map_ui_ = uix.HBox('Parent', obj.layout_, 'BackgroundColor', [0.5 0.5 0.5]);
+            obj.controls_ui_ = uix.HBox('Parent', obj.layout_, 'BackgroundColor', [1 1 1]);
+            obj.layout_.Heights = [24, -1, 36];
             % Create UI buttons
             icon_file = fullfile(obj.root_, 'resources/splash/simiam_splash.png');
             if(isunix)
@@ -134,64 +133,61 @@ classdef AppWindow < handle
                    '</div><br><ol><li>Start by clicking the play button.</li><li>Double-click to send the red robot to a new location.</li><li>Use the mouse to pan and zoom.</li><li>Select the robot to follow it</li><li>If any robot crashes, press the rewind button.</li></ol>' ...
                    '</html>'];
             ui_args = {'Style','pushbutton', 'String', button_string, 'ForegroundColor', 'w', 'FontWeight', 'bold', 'BackgroundColor', obj.ui_colors_.gray, 'Callback', @obj.ui_button_start};
-            ui_parent = obj.layout_.Cell(2,1);
-            obj.logo_ = uicontrol(ui_parent, ui_args{:});
+            obj.logo_ = uicontrol(obj.map_ui_, ui_args{:});
             set(obj.logo_, 'Enable', 'inactive');
             set(findjobj(obj.logo_), 'BorderPainted', 0);
             set(obj.logo_, 'BackgroundColor', [96 184 206]/255);
             
-            ui_args = {'Style','pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_start};
-            ui_parent = obj.layout_.Cell(4,6);
-            play = uicontrol(ui_parent, ui_args{:});
-            obj.ui_set_button_icon(play, 'ui_control_play.png');
-            obj.ui_toggle_control(play, true);
+            % Controls Buttons
+            ui_parent = obj.controls_ui_;
             
-            ui_args = {'Style','pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_reset_simulation};
-            ui_parent = obj.layout_.Cell(4,5);
-            refresh = uicontrol(ui_parent, ui_args{:});
-            obj.ui_set_button_icon(refresh, 'ui_control_reset.png');
-            obj.ui_toggle_control(refresh, false);
-
             ui_args = {'Style','pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_home};
-            ui_parent = obj.layout_.Cell(4,1);
             load = uicontrol(ui_parent, ui_args{:});
             obj.ui_set_button_icon(load, 'ui_control_home.png');
             obj.ui_toggle_control(load, false);
             
+            ui_args = {'Style','pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_reset_simulation};
+            refresh = uicontrol(ui_parent, ui_args{:});
+            obj.ui_set_button_icon(refresh, 'ui_control_reset.png');
+            obj.ui_toggle_control(refresh, false);
+            
+            ui_args = {'Style','pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_start};
+            play = uicontrol(ui_parent, ui_args{:});
+            obj.ui_set_button_icon(play, 'ui_control_play.png');
+            obj.ui_toggle_control(play, true);
+
+            ui_args = {'Style','togglebutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_hardware};
+            hardware = uicontrol(ui_parent, ui_args{:});
+            obj.ui_set_button_icon(hardware, 'ui_control_hardware.png');
+            obj.ui_toggle_control(hardware, true); 
+            
             ui_args = {'Style', 'pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_zoom_in};
-            ui_parent = obj.layout_.Cell(4,11);
             zoom_in = uicontrol(ui_parent, ui_args{:});
             obj.ui_set_button_icon(zoom_in, 'ui_control_zoom_in.png');
             obj.ui_toggle_control(zoom_in, false);
             
             ui_args = {'Style', 'pushbutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_zoom_out};
-            ui_parent = obj.layout_.Cell(4,10);
             zoom_out = uicontrol(ui_parent, ui_args{:});
             obj.ui_set_button_icon(zoom_out, 'ui_control_zoom_out.png');
             obj.ui_toggle_control(zoom_out, false);
             
-            ui_args = {'Style','togglebutton', 'ForegroundColor', 'w', 'FontWeight', 'bold', 'Callback', @obj.ui_button_hardware};
-            ui_parent = obj.layout_.Cell(4,7);
-            hardware = uicontrol(ui_parent, ui_args{:});
-            obj.ui_set_button_icon(hardware, 'ui_control_hardware.png');
-            obj.ui_toggle_control(hardware, true);           
+            % Status
+            ui_parent = obj.status_ui_;
             
             ui_args = {'Style', 'pushbutton', 'BackgroundColor', obj.ui_colors_.gray};
-            ui_parent = obj.layout_.Cell(1,9);
             status = uicontrol(ui_parent, ui_args{:});
             set(status, 'Enable', 'inactive');
             set(findjobj(status), 'BorderPainted', 0);
             obj.ui_set_button_icon(status, 'ui_status_ok.png');
             
             ui_args = {'Style', 'pushbutton', 'BackgroundColor', obj.ui_colors_.gray};
-            ui_parent = obj.layout_.Cell(1,10);
             clock = uicontrol(ui_parent, ui_args{:});
             set(clock, 'Enable', 'inactive');
             set(findjobj(clock), 'BorderPainted', 0);
             obj.ui_set_button_icon(clock, 'ui_status_clock.png');
             
             ui_args = {'Style', 'togglebutton', 'BackgroundColor', obj.ui_colors_.gray};
-            time = uicontrol(obj.layout_.Cell(1,11), ui_args{:});
+            time = uicontrol(ui_parent, ui_args{:});
             set(findjobj(time), 'BorderPainted', 0);
             set(time, 'Value', true);
             
@@ -209,13 +205,11 @@ classdef AppWindow < handle
 
             % Set minimum size for figure
             jFrame = get(handle(obj.parent_), 'JavaFrame');
-            jClient = jFrame.fHG1Client;
+            jClient = jFrame.fHG2Client;
             drawnow;
             jWindow = jClient.getWindow;
             jWindow.setMinimumSize(java.awt.Dimension(800, 600));
-                              
-            Update(obj.layout_);
-            
+                                          
             if(strcmp(obj.origin_, 'simulink') || strcmp(obj.origin_, 'testing'))
                 obj.ui_toggle_control(play, false);
                 obj.ui_toggle_control(refresh, false);
@@ -303,14 +297,11 @@ classdef AppWindow < handle
 
             % Create ui main view
             delete(obj.logo_);
-            view_parent = obj.layout_.Cell(2,1);
-            set(view_parent, 'Children', []);
-            Update(obj.layout_);
             
-            obj.view_ = axes('Parent', obj.layout_.Cell(2,1), ...
+            obj.map_ui_.Children = [];
+            obj.view_ = axes('Parent', obj.map_ui_, ...
                         'ActivePositionProperty','Position', ...
                         'Box', 'on');
-            Update(obj.layout_);            
                     
             % Target Marker
             obj.target_marker_ = plot(obj.view_, 0, 0, ...
@@ -381,9 +372,8 @@ classdef AppWindow < handle
             delete(obj.simulator_);
             delete(obj.view_);
             
-            view_parent = obj.layout_.Cell(2,1);
+            view_parent = obj.map_ui_;
             set(view_parent, 'Children', []);
-            Update(obj.layout_);
             icon_file = fullfile(obj.root_, 'resources/splash/simiam_splash.png');
             if(isunix)
                 icon_url = ['file://' icon_file];
@@ -398,10 +388,10 @@ classdef AppWindow < handle
                              '</div><br><ol><li>Start by clicking the play button.</li><li>Double-click to send the red robot to a new location.</li><li>Use the mouse to pan and zoom.</li><li>Select the robot to follow it</li><li>If any robot crashes, press the rewind button.</li></ol>' ...
                              '</html>'];
             ui_args = {'Style','pushbutton', 'String', button_string, 'ForegroundColor', 'w', 'FontWeight', 'bold', 'BackgroundColor', obj.ui_colors_.gray, 'Callback', @obj.ui_button_start};
-            ui_parent = obj.layout_.Cell(2,1);
+            ui_parent = obj.map_ui_;
             obj.logo_ = uicontrol(ui_parent, ui_args{:});
             set(obj.logo_, 'Enable', 'inactive');
-            set(findjobj(obj.logo_), 'BorderPainted', 0);
+            %set(findjobj(obj.logo_), 'BorderPainted', 0);
             set(obj.logo_, 'BackgroundColor', [96 184 206]/255);
             
             ui_parent_size = get(ui_parent, 'Position');
@@ -485,7 +475,9 @@ classdef AppWindow < handle
         
         function ui_zoom_view(obj, src, event, varargin)
             zoom_level_factor = 0.25;
-            obj.zoom_level_ = obj.zoom_level_+zoom_level_factor*event.VerticalScrollCount;
+            if (~isempty(event.Source.UserData))
+                obj.zoom_level_ = obj.zoom_level_+zoom_level_factor*event.Source.UserData;
+            end
             obj.zoom_level_ = min(max(obj.zoom_level_,0.1), obj.boundary_);
             
             if (~obj.is_tracking_)
@@ -506,12 +498,12 @@ classdef AppWindow < handle
         end
         
         function ui_button_zoom_in(obj, src, event)
-            event.VerticalScrollCount = -1;
+            event.Source.UserData = -1;
             obj.ui_zoom_view(src, event);
         end
         
         function ui_button_zoom_out(obj, src, event)
-            event.VerticalScrollCount = 1;
+            event.Source.UserData = 1;
             obj.ui_zoom_view(src, event);
         end
         
